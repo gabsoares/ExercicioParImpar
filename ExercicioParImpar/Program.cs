228a6a0061fb41ab12e1caa86014d520a8bd221c
gabsoares <soaresgabriel68@gmail.com>
2024-04-18 15:59:03 -0300

    Add project files.

---
+++ ExercicioParImpar/Program.cs
@@ -0,0 +1,26 @@
+﻿int numInicial, numFinal;
+Console.Write("Digite um numero inicial: ");
+numInicial = int.Parse(Console.ReadLine());
+Console.Write("Digite um numero final: ");
+numFinal = int.Parse(Console.ReadLine());
+numInicial++;
+
+do
+{
+    if (numInicial < numFinal)
+    {
+        if (numInicial % 2 == 0)
+        {
+            Console.WriteLine($"Valor par: {numInicial}");
+        }
+        else
+        {
+            Console.WriteLine($"Valor impar: {numInicial}");
+        }
+    }
+    else if (numInicial > numFinal)
+    {
+        Console.WriteLine("Numero final nao pode ser menor ou igual");
+    }
+    numInicial++;
+} while (numInicial <= numFinal - 1);
